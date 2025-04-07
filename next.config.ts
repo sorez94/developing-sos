ab31cf3fba07f5247c5262b56f2a72f7eb38fc7f
Home <soheil>
2025-04-07 20:43:18 +0330

    change

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
-
+const nextConfig: NextConfig = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
 export default withNextIntl(nextConfig);
