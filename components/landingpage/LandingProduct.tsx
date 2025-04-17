a8cd7e5dd356da38528da10917445ebe7b0c408a
Home <soheil>
2025-04-17 18:04:45 +0330

    sos edit

--- components/landingpage/LandingProduct.tsx
+++ components/landingpage/LandingProduct.tsx
@@ -5,9 +5,9 @@ import ShopNowButton from "@/components/ui/shopNowButton";
 const LandingProduct = () => {
     return (
         <>
-            <div className="flex flex-col lg:flex-row h-screen my-32">
-                <div className="lg:w-[30%] w-full flex items-center justify-start px-[60px] bg-white">
-                    <div className="px-6">
+            <div style={{height: 400}} className="flex flex-col lg:flex-row h-screen my-20 px-[100px]">
+                <div className="lg:w-[30%] w-full flex items-center justify-start px-[20px] bg-white">
+                    <div>
                         <h1 className="text-[48px] lg:text-[56px] font-black tracking-wide">Nature</h1>
                         <p className="text-[16px] lg:text-[16px] font-light tracking-widest mb-12">
                             Nature, Green Granite Leather
@@ -15,25 +15,27 @@ const LandingProduct = () => {
                         <ShopNowButton />
                     </div>
                 </div>
-                <div className="lg:w-[70%] w-full">
+                <div className="lg:w-[70%] w-full flex justify-end">
                     <img
                         src="/images/landing-products/p1.png"
+                        width='85%'
                         alt="Landing Product"
-                        className="w-full h-full object-cover"
+                        className=" h-full object-cover"
                     />
                 </div>
             </div>
 
-            <div className="flex flex-col lg:flex-row h-screen my-32">
-                <div className="lg:w-[70%] w-full">
+            <div style={{height: 400}} className="flex flex-col lg:flex-row h-screen my-20 px-[100px]">
+                <div className="lg:w-[70%] w-full flex justify-start">
                     <img
                         src="/images/landing-products/p2.png"
+                        width='85%'
                         alt="Landing Product"
-                        className="w-full h-full object-cover"
+                        className="h-full object-cover"
                     />
                 </div>
-                <div className="lg:w-[30%] w-full flex items-center justify-start px-[60px] bg-white">
-                    <div className="px-6">
+                <div className="lg:w-[30%] w-full flex items-center justify-start px-[20px] bg-white">
+                    <div>
                         <h1 className="text-[48px] lg:text-[56px] font-black tracking-wide">Black Angel</h1>
                         <p className="text-[16px] lg:text-[16px] font-light tracking-widest mb-12">
                             Hard Rock, Granite Leather
@@ -43,9 +45,9 @@ const LandingProduct = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col lg:flex-row h-screen my-32">
-                <div className="lg:w-[30%] w-full flex items-center justify-start px-[60px] bg-white">
-                    <div className="px-6">
+            <div style={{height: 400}} className="flex flex-col lg:flex-row h-screen my-20 px-[100px]">
+                <div className="lg:w-[30%] w-full flex items-center justify-start px-[20px] bg-white">
+                    <div>
                         <h1 className="text-[48px] lg:text-[56px] font-black tracking-wide">Pyramid Bar</h1>
                         <p className="text-[16px] lg:text-[16px] font-light tracking-widest mb-12">
                             Rose, Pink Marble
@@ -53,25 +55,27 @@ const LandingProduct = () => {
                         <ShopNowButton />
                     </div>
                 </div>
-                <div className="lg:w-[70%] w-full">
+                <div className="lg:w-[70%] w-full flex justify-end">
                     <img
                         src="/images/landing-products/p3.png"
+                        width='85%'
                         alt="Landing Product"
-                        className="w-full h-full object-cover"
+                        className="h-full object-cover"
                     />
                 </div>
             </div>
 
-            <div className="flex flex-col lg:flex-row h-screen mt-32 mb-20">
-                <div className="lg:w-[70%] w-full">
+            <div style={{height: 400}} className="flex flex-col lg:flex-row h-screen my-20 px-[100px]">
+                <div className="lg:w-[70%] w-full flex justify-start">
                     <img
                         src="/images/landing-products/p4.png"
+                        width='85%'
                         alt="Landing Product"
-                        className="w-full h-full object-cover"
+                        className="h-full object-cover"
                     />
                 </div>
-                <div className="lg:w-[30%] w-full flex items-center justify-start px-[60px] bg-white">
-                    <div className="px-6">
+                <div className="lg:w-[30%] w-full flex items-center justify-start px-[20px] bg-white">
+                    <div>
                         <h1 className="text-[48px] lg:text-[56px] font-black tracking-wide">Luna</h1>
                         <p className="text-[16px] lg:text-[16px] font-light tracking-widest mb-12">
                             Moon Light, White Marble
