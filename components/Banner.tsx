f38bd3d7c0a5fde124eb1afaab4c5d5764dd1b26
srezaee <@srezaee>
2025-04-21 16:59:41 +0330

    blur header

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 const Banner = () => {
     return (
-        <div className=' flex items-center justify-center w-full h-[500px] bg-[rgb(210,210,208)]'>
+        <div className=' flex items-center justify-center w-full h-[500px] bg-[rgb(202,215,178)]'>
             <h1 className='text-[55px]'> BANNER </h1>
         </div>
     );
