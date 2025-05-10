5ae4006f070e30f239864f3ca1d3ccc552cf8385
srezaee <@srezaee>
2025-05-10 14:19:12 +0330

    about-us page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,18 +1,63 @@
 "use client";
 
+import Banner from "@/components/aboutus/Banner";
+
 export default function Page() {
     return (
-        <section className="px-4 sm:px-6 lg:px-20 py-16 max-w-5xl mx-auto mt-32 text-gray-800">
-            <h1 className="text-4xl font-extrabold mb-6">About Us</h1>
-            <p className="text-lg leading-8 mb-6">
-                <span className="font-semibold text-black">Sense of Stone</span> is a premium brand dedicated to crafting luxurious stone-based products that bring the raw beauty of nature into your living and working spaces.
-            </p>
-            <p className="text-lg leading-8 mb-6">
-                Our mission is to bridge the gap between timeless natural elegance and modern design. With an experienced team of architects, industrial designers, and material engineers, we aim to deliver pieces that are not only visually stunning but also functional and enduring.
-            </p>
-            <p className="text-lg leading-8">
-                We believe that each product tells a story—a story of craftsmanship, elegance, and a deep connection to the earth. With Sense of Stone, transform your space into a sanctuary of serenity and sophistication.
-            </p>
-        </section>
+        <>
+            <Banner/>
+            <section className="py-16 max-w-5xl mx-auto mt-32 text-gray-800">
+                <h1> COMPANY PROFILE </h1>
+                <div className="flex items-center justify-center gap-4">
+                    <img src='/images/about-us/about-us-company.jpg'/>
+                    <div>
+                        <div className='mb-10'>
+                            <h1> ABOUT </h1>
+                            <h1 className='border-b w-fit border-solid border-black'> US </h1>
+                        </div>
+                        <p> Kamer Marble has been established in 1989, is one of the leading companies of Turkish stone
+                            business and specialized in producing the premium quality slabs and variable finished goods
+                            of Marble&Onyx&Travertine. Kamer Marble constantly search for the best blocks especially in
+                            Turkey, beside that, importing the raw materials from all over the world and producing with
+                            high-tech machines and experted craftmanship to reach the best result for the customer
+                            satisfaction.
+
+                            Being an export-oriented company, Kamer Marble has already established a well-reputed name
+                            in the international market for its commitment in quality and exclusive stones. The Goal is
+                            to supply the highest quality products and provide the best customer service to maintain
+                            maximum satisfaction. This has been approved by increasing number of customers in the world.
+
+                            We are extremely proud to move into our newly factory with 26.000 squaremeter closed area in
+                            36.000m2 in total land starting from July 2021.
+                            The new factory provides more space with the larger production and stockyard area and with
+                            additional dry lay area.
+                            The larger, modern and high tech facility allowing us to continue producing great quality
+                            products, increasing in production capacity & quality and customer satisfaction. </p>
+                    </div>
+                </div>
+            </section>
+            <section className="flex w-full">
+                <img className='w-full' src='/images/about-us/mission.jpg'/>
+                <div className='w-full flex flex-col justify-center items-center'>
+                    <div className='mb-10 w'>
+                        <h1 className='border-b w-fit border-solid border-black'> MISSION </h1>
+                    </div>
+                    <p> Kamer Marble is dedicated to producing and exporting the Natural Stone with the highest
+                        quality standards and providing the best customer services by pursuing business through
+                        innovation and advanced technology. </p>
+                </div>
+            </section>
+            <section className="flex py-16 max-w-5xl mx-auto mt-32 text-gray-800">
+                <img src='/images/about-us/vision.jpg'/>
+                <div>
+                    <div className='mb-10'>
+                        <h1 className='border-b w-fit border-solid border-black'> VISION </h1>
+                    </div>
+                    <p>Kamer Marble will the preferred partner in natural stone business providing the highest quality
+                        and exclusive products that exceeds the expectations of our esteemed customers worldwide.</p>
+                </div>
+            </section>
+        </>
+
     );
 }
---
+++ components/aboutus/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Banner = () => {
+    return (
+        <div className='flex items-center justify-center w-full h-[500px] bg-[rgb(202,215,178)]'>
+            <h1 className='text-[55px]'> AboutUs BANNER </h1>
+        </div>
+    );
+};
+
+export default Banner;
