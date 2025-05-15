46d5ee44a1e9564c9d640817586c5d4e62971d78
Home <soheil>
2025-05-15 11:06:19 +0330

    fix landing product in mobile

--- components/landingpage/LandingProduct.tsx
+++ components/landingpage/LandingProduct.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
 import landingProductData from '@/data/landingProductData';
 import { useLocale } from 'next-intl';
 import ShopNowButton from "@/components/ui/shopNowButton";
@@ -14,35 +13,38 @@ const LandingProduct = () => {
             {landingProductData.map((item) => (
                 <div
                     key={item.id}
-                    className="h-[200px] lg:h-[400px] xl:h-[400px] 2xl:h-[400px] flex flex-row my-4 lg:my-20 xl:my-20 2xl:my-20 px-[20px] lg:px-[100px] xl:px-[100px] 2xl:px-[100px] px-[10px]"
+                    className={`
+                        flex 
+                        flex-col-reverse lg:flex-row
+                        ${item.align === 'right' ? 'lg:flex-row-reverse' : 'lg:flex-row'}
+                        my-20 lg:my-20
+                        px-5 lg:px-24
+                        h-auto lg:h-[400px]
+                        bg-white
+                    `}
                 >
-                    {item.align === 'right' && (
-                        <div className="w-[70%] flex justify-start">
-                            <img
-                                src={item.image}
-                                className="w-[85%] h-full object-cover"
-                                alt={item.title}
-                            />
-                        </div>
-                    )}
-
-                    <div className="w-[30%] flex items-center justify-start px-[5px] lg:px-[20px] xl:px-[20px] 2xl:px-[20px] bg-white">
-                        <div>
-                            <h1 className="text-[16px] lg:text-[56px] font-black tracking-wide">{item.title}</h1>
-                            <p className="text-[10px] lg:text-[16px] font-light tracking-widest mb-12">{item.subtitle}</p>
-                            <ShopNowButton href={`/${locale}/product/${item.categoryPath}/${item.slug}`} />
-                        </div>
+                    {/* متن */}
+                    <div className="w-full lg:w-[30%] flex flex-col justify-center px-5 mt-4 lg:mt-0">
+                        <h1 className="text-[16px] lg:text-[56px] font-black tracking-wide">
+                            {item.title}
+                        </h1>
+                        <p className="text-[10px] lg:text-[16px] font-light tracking-widest mb-4 lg:mb-12">
+                            {item.subtitle}
+                        </p>
+                        <ShopNowButton href={`/${locale}/product/${item.categoryPath}/${item.slug}`} />
                     </div>
 
-                    {item.align === 'left' && (
-                        <div className="w-[70%] flex justify-end">
+                    {/* تصویر */}
+                    {/* تصویر */}
+                    <div className="w-full lg:w-[70%] flex justify-center lg:justify-end">
+                        <div className="w-full aspect-[1020/400] lg:aspect-auto">
                             <img
                                 src={item.image}
-                                className="w-[85%] h-full object-cover"
+                                className="w-full h-full object-cover"
                                 alt={item.title}
                             />
                         </div>
-                    )}
+                    </div>
                 </div>
             ))}
         </>
